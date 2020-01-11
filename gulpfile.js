6000961e0b11af587bd80fe45f05f6be33b94fe7
Trang Nguyen <trangnguyen@Trangs-MacBook-Pro.local>
2020-01-11 13:17:54 +0700

    pkg project

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var base64 = require('gulp-base64-inline');
 
-gulp.task('convert', function () {
+function defaultTask() {
     return gulp.src('src/test.csv')
         .pipe(base64('images/',{
             prefix: "",
@@ -9,5 +9,7 @@ gulp.task('convert', function () {
             includeMime: false
         }))
         .pipe(gulp.dest('dist/'));
-    }
-);
+}
+
+
+exports.default = defaultTask
