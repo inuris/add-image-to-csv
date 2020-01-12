0d2afbaf0e7f9ce52b6a2aeae7c369036aaa9761
Trang Nguyen <trangnguyen@Trang-MacBookPro.local>
2020-01-12 21:24:22 +0700

    fix minor

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 
-const inputExt = "csv";
-const imageExt = "jpg,png,gif";
+const INPUT_EXT = "csv";
+const IMAGE_EXT = "jpg,png,gif";
 const SUFFIX = "_converted";
-
-const regex = RegExp('inline\\\(([^\)]+)\\\)', 'g');
+const REGEX = RegExp('inline\\\(([^\)]+)\\\)', 'g');
 
 const dataPath = "./data";
 
 
-glob(dataPath + "/*." + inputExt, function (er, files) {
+glob(dataPath + "/*." + INPUT_EXT, function (er, files) {
   files.forEach(e => {
     let csvPath = e;
     toBase64(csvPath);
@@ -28,7 +27,7 @@ function toBase64(inputPath) {
 
     function inline(inlineExpr, quotedPath) {
       let imageName = quotedPath.replace(/['"]/g, '');
-      let files = glob.sync(dataPath + "/**/" + imageName + ".{" + imageExt + "}");
+      let files = glob.sync(dataPath + "/**/" + imageName + ".{" + IMAGE_EXT + "}");
       if (files) {
         let fileData = fs.readFileSync(files[0]);
         let fileBase64 = Buffer.from(fileData).toString('base64');
@@ -40,11 +39,11 @@ function toBase64(inputPath) {
       }
 
     }
-    if (regex.test(data)) {
+    if (REGEX.test(data)) {
       let ext = path.extname(inputPath);
       let outputPath = path.join(dataPath, path.basename(inputPath, ext) + SUFFIX + ext);
       console.log("Converted: " + inputPath + " >> " + outputPath);
-      let result = data.replace(regex, inline);
+      let result = data.replace(REGEX, inline);
       fs.writeFile(outputPath, result, 'utf8', function (err) {
         if (err) return console.log(err);
       });
